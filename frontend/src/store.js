f447ae71028030902d56aadb5ef40df259b41356
yeojihye <eo1751@hufs-gsuite.kr>
2021-12-12 15:21:47 +0900

    Create-Payment-Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,6 +13,7 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
+    paymentMethod: 'PayPal',
   },
 }
 const reducer = combineReducers({
